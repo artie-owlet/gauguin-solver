e305ff1d868cee6121c9059e79b7212fa99ecb3b
artie-owlet <artie-owlet@protonmail.com>
2025-06-27 11:32:52 +0300

    feat: done with genCombs()

--- src/gen-combs.ts
+++ src/gen-combs.ts
@@ -1,19 +1,81 @@
 import type { NumBox } from './types';
 
-const permuts = (() => {
-    let combs: number[][] = [];
-    for (let i = 0; i < 6; i++) {
-        const newCombs: number[][] = [];
-        for (let n = 1; n <= 6; ++n) {
-            newCombs.push(...combs.filter((comb) => !comb.includes(n)).map((comb) => [...comb, n]));
-        }
-        combs = newCombs;
-    }
-    return combs;
-})();
+const GAME_SIZE = 6;
 
-export function genCombs(numBox: Omit<NumBox, 'combs'>): number[][] {
+export function genCombs({ cellIds, op, result}: Omit<NumBox, 'combs'>): number[][] {
+    switch (op) {
+        case '+':
+            return genAddCombsNonUnique(cellIds.length, result).filter((comb) => isUnique(cellIds, comb));
+        case '-':
+            return genSubCombs(result);
+        case '*':
+            return genMultCombsNonUnique(cellIds.length, result).filter((comb) => isUnique(cellIds, comb));
+        case '/':
+            return genDivCombs(result);
+    }
+    return [[result]];
 }
 
-function genPlusCombs(size: number, result: number): number[][] {
+function genAddCombsNonUnique(size: number, result: number): number[][] {
+    if (size === 1) {
+        return [[result]];
+    }
+    const combs: number[][] = [];
+    for (let i = 1; i <= GAME_SIZE; ++i) {
+        if (i < result) {
+            combs.push(...genAddCombsNonUnique(size - 1, result - i).map((comb) => [i, ...comb]));
+        }
+    }
+    return combs;
+}
+
+function genMultCombsNonUnique(size: number, result: number): number[][] {
+    if (size === 1) {
+        return [[result]];
+    }
+    const combs: number[][] = [];
+    for (let i = 1; i <= GAME_SIZE; ++i) {
+        if (result % i === 0) {
+            combs.push(...genMultCombsNonUnique(size - 1, result / i).map((comb) => [i, ...comb]));
+        }
+    }
+    return combs;
+}
+
+function isUnique(cellIds: number[], comb: number[]): boolean {
+    const coors = cellIds.map((id) => [Math.floor(id / GAME_SIZE), id % GAME_SIZE]);
+    for (let i = 0; i < coors.length - 1; ++i) {
+        for (let j = i + 1; j < coors.length; ++j) {
+            if ((coors[i][0] === coors[j][0] || coors[i][1] === coors[j][1]) && comb[i] === comb[j]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function genSubCombs(result: number): number[][] {
+    const combs: number[][] = [];
+    for (let a = 1; a < GAME_SIZE; ++a) {
+        for (let b = a + 1; b <= GAME_SIZE; ++b) {
+            if (b - a === result) {
+                combs.push([a, b]);
+                combs.push([b, a]);
+            }
+        }
+    }
+    return combs;
+}
+
+function genDivCombs(result: number): number[][] {
+    const combs: number[][] = [];
+    for (let a = 1; a < GAME_SIZE; ++a) {
+        for (let b = a + 1; b <= GAME_SIZE; ++b) {
+            if (b / a === result) {
+                combs.push([a, b]);
+                combs.push([b, a]);
+            }
+        }
+    }
+    return combs;
 }
